1e3ae0a899e63167aa954eb4f75903582c300930
Chaitanya Sharma <schaiten421@gmail.com>
2024-07-27 14:59:31 +0530

    Added attendance system

--- test.py
+++ test.py
@@ -5,7 +5,9 @@ import cv2
 import pickle
 import numpy as np
 import os 
-#37:13
+import csv
+import time
+from datetime import datetime
 video=cv2.VideoCapture(0)
 facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
@@ -17,6 +19,8 @@ with open('data/faces_data.pkl','rb') as f:
 knn=KNeighborsClassifier(n_neighbors=5)
 knn.fit(FACES,LABELS)
 
+COL_NAMES=['NAME','TIME']
+
 while True:
     ret,frame=video.read()
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -25,10 +29,30 @@ while True:
         crop_img=frame[y:y+h,x:x+w,:]
         resized_img=cv2.resize(crop_img,(50,50)).flatten().reshape(1,-1)
         output=knn.predict(resized_img)
+        ts=time.time()
+        date=datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+        timestamp=datetime.fromtimestamp(ts).strftime("%H-%M-%S")
+        exist=os.path.isfile("Attendance/Attendance_"+date+".csv")
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),2)
+        cv2.rectangle(frame,(x,y-40),(x+w,y),(50,50,255),-1)
         cv2.putText(frame,str(output[0]),(x,y-15),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
         cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),1)
+        attendance=[str(output[0]),str(timestamp)]
     cv2.imshow("Frame",frame)
     k=cv2.waitKey(1)
+    if k==ord('o'):
+        if exist:
+            with open("Attendance/Attendance_"+date+".csv","+a") as csvfile:
+                writer=csv.writer(csvfile)
+                writer.writerow(attendance)
+            csvfile.close()
+        else:
+            with open("Attendance/Attendance_"+date+".csv","+a") as csvfile:
+                writer=csv.writer(csvfile)
+                writer.writerow(COL_NAMES)
+                writer.writerow(attendance)
+            csvfile.close()
     if k==ord('q'):
         break
 video.release()
